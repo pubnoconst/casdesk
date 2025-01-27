85d1f678e7b709f06bcfd6570e92e4080b3ef389
pubnoconst <noconst@email.com>
2025-01-28 06:41:42 +1000

    start doing formsview

--- src/formsview.rs
+++ src/formsview.rs
@@ -3,21 +3,27 @@ use crate::root::*;
 
 #[component]
 pub fn Formsview(route: Signal<Route>) -> Element {
-    let mut value = use_signal(String::new);
-
     rsx!(
-        label {
-            "Value: {value}"
-        }
-        Input {
-            value: value.read(),
-            onchange: move |e| {
-                 value.set(e)
+        rect {
+            height: "100%",
+            width: "100%",
+            main_align: "center",
+            cross_align: "center",
+            background: "rgb(0, 119, 182)",
+            color: "white",
+            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+            font_family: "Font1",
+            rect {
+                cross_align: "center",
+                label {
+                    font_size: "50",
+                    "Forms"
+                }
+                Button {
+                    onclick: move |_| route.set(Route::Home),
+                    label { "Back" }
+                }
             }
-        }
-        Button {
-            onclick: move |_| route.set(Route::Home),
-            label { "Back" }
         }
     )
 }
