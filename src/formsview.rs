94ae803d892ce7b9668a99bc3eae6c5fbc405557
pubnoconst <noconst@email.com>
2025-02-03 21:38:37 +1000

    add_formsview

--- src/formsview.rs
+++ src/formsview.rs
@@ -3,6 +3,13 @@ use dioxus_free_icons::icons::fi_icons::FiChevronLeft;
 use dioxus_free_icons::Icon;
 use crate::Route;
 
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Tab {
+    Sale,
+    Purchase,
+    Lease,
+}
+
 #[component]
 pub fn Forms() -> Element {
     let nav = navigator();
@@ -28,20 +35,90 @@ pub fn Forms() -> Element {
                     }
                 }
             }
+            FormBody {}
+        }
+    }
+}
+
+#[component]
+fn FormBody() -> Element {
+    let mut current_tab = use_signal(|| Tab::Sale);
+    rsx! {
+        div {
+            id: "forms-body",
             div {
-                div {
-                    id: "forms-buttons",
-                    button {
-                        "Sale of a device"
-                    }
-                    button {
-                        "Purchase of a device"
-                    }
-                    button {
-                        "Lease a device"
-                    }
+                id: "forms-buttons",
+                button {
+                    onclick: move |_| current_tab.set(Tab::Sale),
+                    "Sale of a device"
+                }
+                button {
+                    onclick: move |_| current_tab.set(Tab::Purchase),
+                    "Purchase of a device"
+                }
+                button {
+                    onclick: move |_| current_tab.set(Tab::Lease),
+                    "Lease a device"
                 }
             }
+            div {
+                match *(current_tab.read()) {
+                    Tab::Sale => rsx! { SaleForm {} },
+                    Tab::Purchase => rsx! { PurchaseForm {} },
+                    Tab::Lease => rsx! { LeaseForm {} },
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn SaleForm() -> Element {
+    rsx! {
+        form {
+            h2 { "Sale Form" }
+            // Customer details inputs
+            label { "Customer Name:" }
+            input { r#type: "text", name: "customer_name", placeholder: "Enter customer name" }
+            br {}
+            // Mobile device details inputs
+            label { "Device Model:" }
+            input { r#type: "text", name: "device_model", placeholder: "Enter device model" }
+            br {}
+            // Add any additional fields as needed...
+            // button { r#type: "submit", "Submit" }
+        }
+    }
+}
+
+#[component]
+fn PurchaseForm() -> Element {
+    rsx! {
+        form {
+            h2 { "Purchase Form" }
+            label { "Customer Name:" }
+            input { r#type: "text", name: "customer_name", placeholder: "Enter customer name" }
+            br {}
+            label { "Device Model:" }
+            input { r#type: "text", name: "device_model", placeholder: "Enter device model" }
+            br {}
+            // button { r#type: "submit", "Submit" }
+        }
+    }
+}
+
+#[component]
+fn LeaseForm() -> Element {
+    rsx! {
+        form {
+            h2 { "Lease Form" }
+            label { "Customer Name:" }
+            input { r#type: "text", name: "customer_name", placeholder: "Enter customer name" }
+            br {}
+            label { "Device Model:" }
+            input { r#type: "text", name: "device_model", placeholder: "Enter device model" }
+            br {}
+            // button { r#type: "submit", "Submit" }
         }
     }
 }
