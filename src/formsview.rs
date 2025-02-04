460124e1f21c7a72f83f346dc09dceb41d7f1a25
pubnoconst <noconst@email.com>
2025-02-04 14:33:34 +1000

    add_form_fields

--- src/formsview.rs
+++ src/formsview.rs
@@ -86,8 +86,58 @@ fn SaleForm() -> Element {
             }
             div {
                 class: "form-row",
-                label { "Device Model:" }
-                input { r#type: "text", name: "device_model", placeholder: "Enter device model" }
+                label { "Device:" }
+                input { r#type: "text", name: "device_model", placeholder: "Name, model, model number" }
+            }
+            div {
+                class: "form-row",
+                label { "Device Color:" }
+                input { r#type: "text", name: "device_color", placeholder: "Enter device color" }
+            }
+            div {
+                class: "form-row",
+                label { "IMEI (or the like):" }
+                input { r#type: "text", name: "device_imei", placeholder: "*#06*#" }
+            }
+            div {
+                class: "form-row",
+                label { "Locked to provider:" }
+                input { r#type: "text", name: "device_provider", placeholder: "Unlocked/Optus..." }
+            }
+            div {
+                class: "form-row",
+                label { "Device Price AUD $:" }
+                input { r#type: "text", name: "device_price", placeholder: "Enter device price" }
+            }
+            div {
+                class: "form-row",
+                label { "Payment Method:" }
+                input { r#type: "text", name: "payment_method", placeholder: "EFTPOS/Cash" }
+            }
+            div {
+                class: "form-row",
+                label { "Customer's contact number:" }
+                input { r#type: "text", name: "customers_contact_number", placeholder: "Enter phone number" }
+            }
+            div {
+                class: "form-row",
+                label { "Customer's address:" }
+                input { r#type: "text", name: "customer_addr", placeholder: "Enter customer's address" }
+            }
+            div {
+                class: "form-row",
+                label { "Customer's ID number:" }
+                input { r#type: "text", name: "customer_id", placeholder: "Enter customer's ID" }
+            }
+            div {
+                class: "form-row",
+                label { "Staff name:" }
+                input { r#type: "text", name: "stuff_name", placeholder: "Enter staff name" }
+            }
+            div {
+                class: "form-row",
+                label { "date" }
+                input { r#type: "text", name: "date_of_sale", placeholder: "MM/DD/YY" }
             }
         }
     }
@@ -101,13 +151,58 @@ fn PurchaseForm() -> Element {
             class: "form-div",
             div {
                 class: "form-row",
-                label { "Customer Name:" }
-                input { r#type: "text", name: "customer_name", placeholder: "Enter customer name" }
+                label { "Seller's Name:" }
+                input { r#type: "text", name: "seller_name", placeholder: "Enter seller's name" }
             }
             div {
                 class: "form-row",
-                label { "Device Model:" }
-                input { r#type: "text", name: "device_model", placeholder: "Enter device model" }
+                label { "Device:" }
+                input { r#type: "text", name: "device_model", placeholder: "Name, model, model number" }
+            }
+            div {
+                class: "form-row",
+                label { "Device Color:" }
+                input { r#type: "text", name: "device_color", placeholder: "Enter device color" }
+            }
+            div {
+                class: "form-row",
+                label { "IMEI (or the like):" }
+                input { r#type: "text", name: "device_imei", placeholder: "*#06*#" }
+            }
+            div {
+                class: "form-row",
+                label { "Locked to provider:" }
+                input { r#type: "text", name: "device_provider", placeholder: "Unlocked/Optus..." }
+            }
+            div {
+                class: "form-row",
+                label { "Purchase price AUD $:" }
+                input { r#type: "text", name: "purchase_price", placeholder: "Enter purchase price" }
+            }
+            div {
+                class: "form-row",
+                label { "Seller's contact number:" }
+                input { r#type: "text", name: "sellers_contact_number", placeholder: "Enter phone number" }
+            }
+            div {
+                class: "form-row",
+                label { "Seller's address:" }
+                input { r#type: "text", name: "seller_addr", placeholder: "Enter seller's address" }
+            }
+            div {
+                class: "form-row",
+                label { "Seller's ID number:" }
+                input { r#type: "text", name: "seller_id", placeholder: "Enter customer ID" }
+            }
+            div {
+                class: "form-row",
+                label { "Staff name:" }
+                input { r#type: "text", name: "stuff_name", placeholder: "Enter staff name" }
+            }
+            div {
+                class: "form-row",
+                label { "date" }
+                input { r#type: "text", name: "date_of_sale", placeholder: "MM/DD/YY" }
             }
         }
     }
