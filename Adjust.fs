38eeb57e06e63e31b4d25e85228b1a7704bf1ecb
pubnoconst <noconst@email.com>
2025-02-17 14:48:27 +1000

    figure_out_grid

--- Adjust.fs
+++ Adjust.fs
@@ -6,18 +6,34 @@ open Avalonia.Layout
 open Pages
 
 let view (navigateTo: Page -> unit) =
-        StackPanel.create [
-            StackPanel.verticalAlignment VerticalAlignment.Center
-            StackPanel.horizontalAlignment HorizontalAlignment.Center
-            StackPanel.spacing 20
-            StackPanel.children [
-                TextBlock.create [
-                    TextBlock.text "Adjust POS Page"
-                    TextBlock.fontSize 48
-                ]
-                Button.create [
-                    Button.content "Back to Home"
-                    Button.onClick (fun _ -> navigateTo Home)
+    Grid.create [
+        Grid.rowDefinitions "Auto, *"
+        Grid.margin 25
+        Grid.children [
+            // nav grid
+            Grid.create [
+                Grid.verticalAlignment VerticalAlignment.Center
+                Grid.columnDefinitions "Auto, *, Auto"
+                Grid.children [
+                    Button.create [
+                        Grid.column 0
+                        Button.content "Back"
+                        Button.onClick (fun _ -> navigateTo Home)
+                    ]
+                    StackPanel.create [
+                        Grid.column 1
+
+                    ]
+                    TextBlock.create [
+                        Grid.column 2
+                        TextBlock.text "Adjust POS"
+                        TextBlock.fontSize 24
+                    ]
                 ]
             ]
+            // content grid
+            Grid.create [
+
+            ]
         ]
+    ]
