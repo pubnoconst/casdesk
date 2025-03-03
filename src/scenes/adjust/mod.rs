e1ac9eed3a3abe6eac1032a51f89839b29066009
pubnoconst <noconst@email.com>
2025-03-03 22:27:06 +1000

    adjust

--- src/scenes/adjust/mod.rs
+++ src/scenes/adjust/mod.rs
@@ -1,14 +1,60 @@
+use std::str::FromStr;
 use dioxus::prelude::*;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
+
+const CSS: &str = include_str!("adjust.css");
 
 #[component]
 pub fn Adjust() -> Element {
+    let mut software = use_signal(|| dec!(0));
+    let mut machine = use_signal(|| dec!(0));
+
+    let extra = use_memo(move || *machine.read() - *software.read());
+
+    let missing_extra = use_memo(move || {
+        (*extra.read() - *extra.read() * dec!(0.009))
+            .round_dp(2)
+    });
+    let missing_extra_neg = use_memo(move || dec!(-1) * *missing_extra.read());
+
     rsx! {
+        style { "{CSS}" }
         div {
             class: "frame",
             super::NavBar { page_title: "Adjust POS" }
             div {
-                "placeholder"
+                id: "content",
+                div {
+                    class: "card-container",
+                    div {
+                        class: "card",
+                        "EFTPOS Total in Mybug"
+                        input {
+                            r#type: "number",
+                            value: "{software}",
+                            oninput: move |e| software.set(Decimal::from_str(&e.value()).unwrap_or(dec!(0)))
+                        }
+                    }
+                    div {
+                        class: "card",
+                        "EFTPOS Machine Total" 
+                        input {
+                            r#type: "number",
+                            value: "{machine}",
+                            oninput: move |e| machine.set(Decimal::from_str(&e.value()).unwrap_or(dec!(0)))
+                        }
+                    }
+                }
+                div {
+                    class: "summary",
+                    if *software.read() > *machine.read() {
+                        "You are missing ${missing_extra_neg} sales from the EFTPOS Machine ✔️"
+                    } if *software.read() < *machine.read() {
+                        "You recorded ${missing_extra} extraneous sales in Mybug ⚠️"
+                    }
+                }
             }
         }
-    }
+    }    
 }
