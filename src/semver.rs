7ed9c56fd826c39e3e4ec8f00342c1935d15a95a
pubnoconst <noconst@email.com>
2025-03-10 20:08:13 +1000

    semverCmp

--- src/semver.rs
+++ src/semver.rs
@@ -10,59 +10,27 @@ impl SemverCmp for &str {
     }
 
     fn is_bigger_than(&self, other: &Self) -> bool {
-        if self.is_equal_to(other) {
-            return false;
-        }
-        let lhs = self
-            .split('.')
-            .map(|s| {
-                s.parse::<u32>()
-                    .expect(&format!("Semver format error {}", self))
-            })
-            .collect::<Vec<_>>();
-        let rhs = other
-            .split('.')
-            .map(|s| {
-                s.parse::<u32>()
-                    .expect(&format!("Semver format error {}", other))
-            })
-            .collect::<Vec<_>>();
-
-        if lhs[2] > rhs[2] {
-            return true;
-        }
-        if lhs[1] > rhs[1] {
-            return true;
-        }
-        if lhs[0] > rhs[0] {
-            return true;
-        }
-        return false;
+        let (lhs, rhs) = (parse_semver(self), parse_semver(other));
+        lhs > rhs
     }
 
     fn is_smaller_than(&self, other: &Self) -> bool {
-        if self.is_equal_to(other) {
-            return false;
-        }
-        let lhs = self
-            .split('.')
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect::<Vec<_>>();
-        let rhs = other
-            .split('.')
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect::<Vec<_>>();
-        if lhs[2] < rhs[2] {
-            return true;
-        }
-        if lhs[1] < rhs[1] {
-            return true;
-        }
-        if lhs[0] < rhs[0] {
-            return true;
-        }
-        return false;
+        let (lhs, rhs) = (parse_semver(self), parse_semver(other));
+        lhs < rhs
     }
+}
+
+fn parse_semver(semver: &str) -> (u32, u32, u32) {
+    let parts: Vec<u32> = semver
+        .split('.')
+        .map(|s| s.parse::<u32>().expect(&format!("Invalid semver format: {}", semver)))
+        .collect();
+
+    if parts.len() != 3 {
+        panic!("Semver must have exactly three parts: {}", semver);
+    }
+
+    (parts[0], parts[1], parts[2])
 }
 
 #[cfg(test)]
@@ -74,5 +42,7 @@ mod tests {
         assert!("1.2.3".is_equal_to(&"1.2.3"));
         assert!("1.2.3".is_bigger_than(&"1.2.0"));
         assert!("1.2.3".is_smaller_than(&"1.2.4"));
+        assert!("2.0.0".is_bigger_than(&"1.11.9"));
+        assert!("0.1.0".is_smaller_than(&"0.1.1"));
     }
 }
