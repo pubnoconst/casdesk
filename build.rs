8a786c45733356b9d583f2eff5d08be305c5b215
pubnoconst <noconst@email.com>
2025-03-12 14:11:26 +1000

    createOutputInBuildTime

--- build.rs
+++ build.rs
@@ -5,18 +5,23 @@ const OUT_DIR: &str = "assets/output";
 fn load_font() {
     let figtree_font_bytes = include_bytes!("assets/Figtree.ttf");
     let figtree_encoded = rbase64::encode(figtree_font_bytes);
-    let font_out_path = Path::new(&OUT_DIR).join("Figtree.ttf.bin");
+    let font_out_path = Path::new(OUT_DIR).join("Figtree.ttf.bin");
     fs::write(font_out_path, figtree_encoded).expect("WRITE FAILURE: font");
 }
 
 fn load_logo() {
     let logo_bytes = include_bytes!("assets/logobanner.png");
     let logo_encoded = rbase64::encode(logo_bytes);
-    let logo_out_path = Path::new(&OUT_DIR).join("logobanner.png.bin");
+    let logo_out_path = Path::new(OUT_DIR).join("logobanner.png.bin");
     fs::write(logo_out_path, logo_encoded).expect("WRITE FAILURE: logo");
 }
 
 fn main() {
+    // Create the output directory if it doesn't exist
+    if !Path::new(OUT_DIR).exists() {
+        fs::create_dir_all(OUT_DIR).expect("Failed to create output directory");
+    }
+
     load_font();
     load_logo();
 }
